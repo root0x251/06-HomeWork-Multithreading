a73cad697e898ad065b9e28f997aee6f413aaddc
Slava <rtootik@gmail.com>
2017-03-11 11:51:25 +0300

    Initial Commit

---
+++ 06-HomeWork-Multithreading/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  06-HomeWork-Multithreading
+//
+//  Created by Slava on 11.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
